0311d01618808aa8ea54510e4cb27ff60a92dcff
ryannarnoldd <ryannarnoldd@gmail.com>
2025-02-04 18:35:59 -0500

    Small UI deletion button changes

--- client/src/components/CollectionCard.tsx
+++ client/src/components/CollectionCard.tsx
@@ -28,10 +28,10 @@ const CollectionCard: React.FC<CollectionCardProps> = ({ collection }) => {
   const [deleteCollection] = useMutation(DELETE_COLLECTION, { refetchQueries: [QUERY_ME, 'Me'] });
   const [deleteItem] = useMutation(DELETE_ITEM, { refetchQueries: [QUERY_ME, 'Me'] });
 
-const handleDeleteCollection = async () => {
+  const handleDeleteCollection = async () => {
     try {
-      
-        await deleteCollection({ variables: { collectionId: collection._id } });
+
+      await deleteCollection({ variables: { collectionId: collection._id } });
 
     } catch (err) {
       console.error('Error deleting collection:', err);
@@ -42,21 +42,21 @@ const handleDeleteCollection = async () => {
     <Card>
       <Card.Img variant="top" src={collection.image} alt={collection.title} />
       <Card.Body>
-        <Card.Title>{collection.title} ${100} {" . "}
-            <Button onClick={() => handleDeleteCollection()} variant="danger">Delete</Button>
+        <Card.Title>{collection.title} ${100}
+          <Button onClick={() => handleDeleteCollection()} variant="danger">Delete</Button>
         </Card.Title>
         <Card.Text>{collection.description}</Card.Text>
-        
+
         {/* Display collection items */}
         <ul>
           {collection.items.map((item) => (
             <b key={item._id}>
-                {/* Add a small tiny X button that will delete the item. */}
-                <li> {item.name} ${item.price} 
-                    <Button onClick={async () => {
-                        await deleteItem({ variables: { collectionId: collection._id, itemId: item._id } })
-                    }} variant="danger">X</Button>
-                </li>
+              {/* Add a small tiny X button that will delete the item. */}
+              <li> {item.name} ${item.price}
+                <Button onClick={async () => {
+                  await deleteItem({ variables: { collectionId: collection._id, itemId: item._id } })
+                }} variant="danger">X</Button>
+              </li>
             </b>
           ))}
         </ul>
