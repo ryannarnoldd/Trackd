fa1c5429bcc00e600e4acc08e9c7cfd7795ebc7b
Don Wayn <dondrecaine@MacBook-Air.local>
2025-02-03 22:11:39 -0500

    server running for new account creation

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -20,7 +20,6 @@ export const ADD_USER = gql`
         _id
         username
         email
-        password
       }
     }
   }
--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/trackd');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/trackd');
 
 const db = mongoose.connection;
 
